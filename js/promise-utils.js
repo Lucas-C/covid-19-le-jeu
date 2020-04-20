c4ad226ada5d11de56aac84fb87ec3c2a81ee7aa
Lucas Cimon <lucas.cimon@gmail.com>
2020-04-20 19:48:03 +0200

    Ajout de commentaires

--- js/promise-utils.js
+++ js/promise-utils.js
@@ -1,11 +1,13 @@
 import { delay, MS } from './timing.js';
 
+// Temporise les déplacements des pions pour permettre à l'animation de se déclencher
 export function wrapAnimDelay(callback) {
   return delay({ ms: MS.ADD_PAWN_DELAY })() // On délaie légèrement l'ajout à chaque planète pour déclencher l'animation CSS
     .then(callback)
     .then(delay({ ms: MS.PAWN_MOVE_ANIMATION_DURATION })); // On attend la fin de l'animation CSS
 }
 
+// Exécute les fonctions d'un tableau (array) l'une après l'autre
 export function chainExec(funcs) {
   let startChain = null;
   let promise = new Promise((resolve) => {
