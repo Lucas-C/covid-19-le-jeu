40277a52d71d29601d46de1187e43fd9a9090ae0
D HD <delphin.darraud@gmail.com>
2020-04-26 10:43:29 +0200

    step4

--- js/turn-step-4.js
+++ js/turn-step-4.js
@@ -12,10 +12,11 @@ export class TurnStep4 extends TurnStep {
     return '2e déplacement des habitant.e.s';
   }
 }
-// Retour à la maison
+// Retour à la maison ** NE FONCTIONNE PAS **
 function returnHome(board) {// ordre : sick, incubating, sane, healed => extractPawns(count,2)
   const pawnsA = board.robotAcademy.extractAllPawns() ;
   const pawnsB = board.batterieMarket.extractAllPawns() ;
+  console.log('Retour de '+pawnsA.lenght+' pions de la robot académie');
   return chainExec(pawnsA.map((pawn) => 
     board.planetTokenAcquirePawn(pawn),
   )).then(chainExec(pawnsB.map((pawn) => 
