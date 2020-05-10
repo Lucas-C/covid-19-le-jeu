7e3f8028114bfccb0218ce266959fc97e3efa2e6
Delphin <63443223+delphinbzh@users.noreply.github.com>
2020-05-11 01:10:43 +0200

    Splash (#51)
    * Overlay screen + bug goTobopital
    
    * addSlot

--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,4 @@
-
+import { wrapAnimDelay } from './promise-utils.js';
 // Ajoute les classes CSS correspondant aux animations animate.css
 export function animateCSS(node, animationName, callback) {
   node.classList.add('animated', animationName);
@@ -10,4 +10,23 @@ export function animateCSS(node, animationName, callback) {
     }
   }
   node.addEventListener('animationend', handleAnimationEnd);
+}
+
+// Autre fonction d'animations
+export class SplashOverlay {
+  constructor(doc, eltName) {
+    this.overlayElem = doc.getElementById(eltName);
+  }
+  toggleDisplay() {
+    return wrapAnimDelay(() => {
+      if (this.overlayElem.style.display === 'none') {
+        this.overlayElem.style.display = 'flex';
+        animateCSS(this.overlayElem, 'slideInDown');
+      } else {
+        animateCSS(this.overlayElem, 'slideOutUp', () => {
+          this.overlayElem.style.display = 'none';
+        });
+      }
+    });
+  }
 }
